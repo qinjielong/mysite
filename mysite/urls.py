7fd372c59bbc0e56117fdd66a61dfa6d6a2a854b
qinjielong <qinjielong@126.com>
2021-06-14 19:43:26 +0800

    django网站制作完成

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('article/', include('article.urls', namespace='article')),
     path('home/', TemplateView.as_view(template_name="home.html"), name='home'),
     path('image/', include('image.urls', namespace='image')),
-    #path('course/', include('course.urls', namespace='course')),
+    path('course/', include('course.urls', namespace='course')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
